ec21d93bce9e930b7f19fac7c6daeef1fc6d68b5
Michael Penrow <106648+mpenrow@users.noreply.github.com>
2022-08-12 16:37:57 -0400

    Added dependency to external api call.

--- BitcoinConverter/BitcoinConverter.Code/ConverterSvc.cs
+++ BitcoinConverter/BitcoinConverter.Code/ConverterSvc.cs
@@ -1,31 +1,44 @@
-﻿namespace CloudAcademy.Bitcoin;
+﻿using System.Text;
+using System.Text.Json;
+
+namespace CloudAcademy.Bitcoin;
 public class ConverterSvc
 {
+  private const string BITCOIN_CURRENTPRICE_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
   public ConverterSvc()
   {
 
   }
 
-  public int GetExchangeRate(string currency)
+  public async Task<double> GetExchangeRate(string currency)
   {
+    var response = await new HttpClient().GetStringAsync(BITCOIN_CURRENTPRICE_URL);
+
     if (currency.Equals("USD"))
     {
-      return 100;
+      var jsonDoc = JsonDocument.Parse(Encoding.ASCII.GetBytes(response));
+      var rate = jsonDoc.RootElement.GetProperty("bpi").GetProperty(currency).GetProperty("rate");
+      return Double.Parse(rate.GetString());
     }
     else if (currency.Equals("GBP"))
     {
-      return 200;
+      var jsonDoc = JsonDocument.Parse(Encoding.ASCII.GetBytes(response));
+      var rate = jsonDoc.RootElement.GetProperty("bpi").GetProperty(currency).GetProperty("rate");
+      return Double.Parse(rate.GetString());
     }
     else if (currency.Equals("EUR"))
     {
-      return 300;
+      var jsonDoc = JsonDocument.Parse(Encoding.ASCII.GetBytes(response));
+      var rate = jsonDoc.RootElement.GetProperty("bpi").GetProperty(currency).GetProperty("rate");
+      return Double.Parse(rate.GetString());
     }
     return 0;
   }
 
-  public int ConvertBitcoins(string currency, int coins)
+  public async Task<double> ConvertBitcoins(string currency, int coins)
   {
-    var exchangeRate = GetExchangeRate(currency);
+    var exchangeRate = await GetExchangeRate(currency);
     return exchangeRate * coins;
   }
 }
