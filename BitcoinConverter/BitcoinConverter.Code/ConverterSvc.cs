8f1c685b62e429d199b0fd40670d4dc5b7da1aa8
Michael Penrow <106648+mpenrow@users.noreply.github.com>
2022-08-12 13:16:00 -0400

    Initial Project creation and GetExchangeRate implementation

---
+++ BitcoinConverter/BitcoinConverter.Code/ConverterSvc.cs
@@ -0,0 +1,25 @@
+﻿namespace CloudAcademy.Bitcoin;
+public class ConverterSvc
+{
+  public ConverterSvc()
+  {
+
+  }
+
+  public int GetExchangeRate(string currency)
+  {
+    if (currency.Equals("USD"))
+    {
+      return 100;
+    }
+    else if (currency.Equals("GBP"))
+    {
+      return 200;
+    }
+    else if (currency.Equals("EUR"))
+    {
+      return 300;
+    }
+    return 0;
+  }
+}
